40a39b7dbc69d9037ec82122b825a8fa16d3112b
Jakob Ryden <jakob.bohem@gmail.com>
2013-04-23 23:18:17 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keepscore::Application.config.secret_token = '3595daaaaa6a02151090e0a9499ea2ab9be689cbf110e806d55331f17af8547c5d3987bbcee0102e1a8c97178f26ef0cdb541049c1292d772c52d9c68df296ba'
